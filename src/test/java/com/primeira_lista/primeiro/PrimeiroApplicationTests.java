60ea0fa6be1bcb3cc857c24088ffb537fcac0f14
SabrinaSouzaDev <lynxsabri@gmail.com>
2025-03-18 09:22:23 -0300

    configurações do projeto spring boot com java

---
+++ src/test/java/com/primeira_lista/primeiro/PrimeiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primeira_lista.primeiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
